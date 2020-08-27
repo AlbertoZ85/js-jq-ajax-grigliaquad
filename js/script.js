8e5d30378dd0531e42bd1387c5eb3e19802d894c
Alberto Zordan <zorberto@gmail.com>
2020-08-27 17:31:27 +0200

    AJAX done

--- js/script.js
+++ js/script.js
@@ -3,9 +3,35 @@
 // Il numero ottenuto appare al centro del quadrato.
 
 $(document).ready(function () {
+    // Creazione dinamica dei quadrati
     for (var i = 0; i < 36; i++) {
         $('.container').append('<div class="square"></div>')
     }
 
+    // Sperimento il metodo .one() -> come .on() ma il gestore eventi è eseguito al più una volta per elemento e per tipo di evento
+    $('.square').one('click', function () {
+        var quadrato = $(this);
 
+        // Chiamata AJAX
+        $.ajax(
+            {
+                url: "https://flynn.boolean.careers/exercises/api/random/int",
+                method: "GET",
+                success: function (obj) {
+                    quadrato.text(obj.response);
+                    // if (obj.response <= 5) {
+                    //     quadrato.addClass('giallo');
+                    // } else {
+                    //     quadrato.addClass('verde');
+                    // }
+
+                    // Operatore ternario in sostituzione al 'if-else'
+                    obj.response <= 5 ? quadrato.addClass('giallo') : quadrato.addClass('verde');
+                },
+                error: function () {
+                    alert("È avvenuto un errore.")
+                }
+            }
+        );
+    });
 });
